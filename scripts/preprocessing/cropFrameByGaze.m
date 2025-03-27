fbc98b08f06a970dd3b341fa6dd3bc2ddf2ab0cc
AJ Haskins <43219681+ajhaskins@users.noreply.github.com>
2025-03-27 15:05:54 -0700

    Preprocessing updates
    Add max block duration switch; add gaze tile cropping function

--- scripts/preprocessing/cropFrameByGaze.m
+++ scripts/preprocessing/cropFrameByGaze.m
@@ -1,33 +1,53 @@
 % Use gaze coordinates from HMET to crop video frames 
-% ROUGH SKETCH, needs cleaned up
-% ajh 2.18.25
+% last updated 3.26.25
+% ajh 
 
-clear all; clc;
-% Fixations file (timing in ns)
-fixData = readtable('/Users/visuallearninglab/Desktop/Timeseries Data + Scene Video (3)/2025-01-17_11-33-01-dc097c0a/fixations.csv','Delimiter',',');
+function gazeTileArray = cropFrameByGaze(fixData, worldTimestamps, v, startStopMatrix)
 
-% Video timestamps file (timing in ns)
-worldTimestamps = readtable('/Users/visuallearninglab/Desktop/Timeseries Data + Scene Video (3)/2025-01-17_11-33-01-dc097c0a/world_timestamps.csv','Delimiter',',');
-worldTimestamps.timestamp_sec_ = worldTimestamps.timestamp_ns_/1000000000;
-worldTimestamps.time_elapsed_ = worldTimestamps.timestamp_sec_(:) - worldTimestamps.timestamp_sec_(1);
+% Subset the fixation data to include only the relevant windows
 
-% Video file
-v = VideoReader('/Users/visuallearninglab/Desktop/Timeseries Data + Scene Video (3)/2025-01-17_11-33-01-dc097c0a/7757d56a_0.0-1153.477.mp4');
-
-% Time window in seconds
-thisObjectTimes = 283:307.5;
-theseTimestamps = worldTimestamps.timestamp_ns_(find(worldTimestamps.time_elapsed_ >= thisObjectTimes(1) & worldTimestamps.time_elapsed_ <= thisObjectTimes(end)));
-
-% Fixation table for this time window
-fixDataSubset = fixData(fixData.startTimestamp_ns_ >= theseTimestamps(1) & fixData.startTimestamp_ns_ <= theseTimestamps(end),:);
+for thisWindow = 1:height(startStopMatrix) % the n of the n-by-2 matrix
+    if thisWindow == 1
+        fixDataSubset = fixData(fixData.startTimestamp_ns_ >= startStopMatrix(thisWindow,1) & fixData.startTimestamp_ns_ <= startStopMatrix(thisWindow,2),:);
+    elseif thisWindow >1
+        fixDataSubset = [fixDataSubset;...
+            fixData(fixData.startTimestamp_ns_ >= startStopMatrix(thisWindow,1) & fixData.startTimestamp_ns_ <= startStopMatrix(thisWindow,2),:)];
+    end
+end
 
 % Relevant video frame indices
 frameInds = [];
-for fixNum = 1:height(fixDataSubset) 
+for fixNum = 1:height(fixDataSubset) % for each fixation (row)
     thisFrameInd = find(worldTimestamps.timestamp_ns_> fixDataSubset.startTimestamp_ns_(fixNum) & worldTimestamps.timestamp_ns_ < fixDataSubset.endTimestamp_ns_(fixNum));
-    frameInds = [frameInds; thisFrameInd(1) ];
+    if isempty(thisFrameInd) % this may happen for very short fixations in lower frame rate vids 
+        frameInds = [frameInds; nan ];
+    else
+        frameInds = [frameInds; thisFrameInd(1) ]; % video frames
+    end
 end
 
+% Make the array for gaze tiles
+gazeTileArray = cell([1 height(frameInds)]); %nan(200,200,3,height(frameInds));
+
+for thisFrameInd = 1:height(frameInds)
+    if isnan(frameInds(thisFrameInd))
+        gazeTileArray(1,thisFrameInd) = {nan(200,200,3)};
+    else
+        rows = round(fixDataSubset.fixationY_px_(thisFrameInd)-99):round(fixDataSubset.fixationY_px_(thisFrameInd)+100);
+        cols = round(fixDataSubset.fixationX_px_(thisFrameInd)-99):round(fixDataSubset.fixationX_px_(thisFrameInd)+100);
+        if max(rows) > 1200 || max(cols) > 1600 % fixation was very eccentric & we can't crop an adequate tile
+            gazeTileArray(1,thisFrameInd) = {nan(200,200,3)};
+        else
+            frame = read(v,frameInds(thisFrameInd));
+            frame = (frame(rows,cols,:)); %imshow(frame)
+      
+            gazeTileArray{1,thisFrameInd} = frame;
+        end
+    end
+end
+
+
+% SANITY CHECKING
 % Plot fixation on frames, uncropped
  
 % for fixNum = 1:height(fixDataSubset) 
@@ -39,22 +59,22 @@ end
 % end
 
 % save the tiles to a folder
-saveFolder = 'zero/data/gaze-tiles/';
-
-t = tiledlayout;
-t.TileSpacing = 'compact';
-t.Padding = 'compact';
-set(gcf,'Color','white')
-
-for fixNum = 1:height(fixDataSubset)
-    rows = round(fixDataSubset.fixationY_px_(fixNum)-99):round(fixDataSubset.fixationY_px_(fixNum)+100);
-    cols = round(fixDataSubset.fixationX_px_(fixNum)-99):round(fixDataSubset.fixationX_px_(fixNum)+100);
-    frame = read(v,frameInds(fixNum));
-    %subplot(7,8,fixNum)
-    nexttile
-    imshow(frame(rows,cols,:));
-    imwrite(frame(rows,cols,:),[saveFolder 'test-obj1-tile' num2str(fixNum) '.jpeg'])
-    hold on
-    scatter(100,100,500,'red','LineWidth',4,'MarkerEdgeAlpha',.4)
-end
-
+% saveFolder = 'zero/data/gaze-tiles/';
+% 
+% t = tiledlayout;
+% t.TileSpacing = 'compact';
+% t.Padding = 'compact';
+% set(gcf,'Color','white')
+% 
+% for fixNum = 1:height(fixDataSubset)
+%     rows = round(fixDataSubset.fixationY_px_(fixNum)-99):round(fixDataSubset.fixationY_px_(fixNum)+100);
+%     cols = round(fixDataSubset.fixationX_px_(fixNum)-99):round(fixDataSubset.fixationX_px_(fixNum)+100);
+%     frame = read(v,frameInds(fixNum));
+%     %subplot(7,8,fixNum)
+%     nexttile
+%     imshow(frame(rows,cols,:));
+%     %imwrite(frame(rows,cols,:),[saveFolder 'test-obj1-tile' num2str(fixNum) '.jpeg'])
+%     hold on
+%     scatter(100,100,500,'red','LineWidth',4,'MarkerEdgeAlpha',.4)
+% end
+% 
