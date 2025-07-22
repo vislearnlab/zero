17814912d44a2cb58eaa7ac5338e9e91404f330c
AJ Haskins <43219681+ajhaskins@users.noreply.github.com>
2025-07-22 16:30:19 -0700

    Updated scripts for computing transcript accuracy
    1) Make sure chunks are evenly sized before embedding, 2) filter out "um", "uh", "okay", 3) truncate descriptions at max 400 tokens

--- scripts/preprocessing/pp_transcripts/compareAutoHumanAccRatings.m
+++ scripts/preprocessing/pp_transcripts/compareAutoHumanAccRatings.m
@@ -16,15 +16,15 @@ automatedFile = readtable([projectDir ...
 % Create a table/CSV combining automated vs. human-rated accuracy on subset
 
 % Loop through each of the human-rated descriptions
-newSubsetTable = table('Size',[0 6], 'VariableTypes', {'string','string','double',...
-    'double','double','double'},'VariableNames',{'SubjectID','ObjectID','TalkBlock',...
-    'HumanAcc1','HumanAcc2','AutoAcc'});
+newSubsetTable = table('Size',[0 7], 'VariableTypes', {'string','string','double',...
+    'double','double','double','double'},'VariableNames',{'SubjectID','ObjectID','TalkBlock',...
+    'HumanAcc1','HumanAcc2','AutoAcc','Tokens'});
 
 for thisDescription = 1:height(ratingsFile)
     thisDescRowInAutomated = find(...
         contains(automatedFile.subject_name, ratingsFile.SubjectID(thisDescription)) & ...
         contains(automatedFile.object_name, ratingsFile.ObjectID(thisDescription)) & ...
-        ratingsFile.TalkBlock(thisDescription) == automatedFile.talk_block);
+        ratingsFile.TalkBlock(thisDescription) == automatedFile.block_number);
 
     if isempty(thisDescRowInAutomated)
         continue
@@ -36,7 +36,8 @@ for thisDescription = 1:height(ratingsFile)
         HumanAcc1 = ratingsFile.accuracy_Nicole(thisDescription);
         HumanAcc2 = ratingsFile.accuracy_Tarun(thisDescription);
         AutoAcc = automatedFile.cosine_similarity_to_gt(thisDescRowInAutomated);
-        newSubsetTable = [newSubsetTable; table(SubjectID,ObjectID,TalkBlock,HumanAcc1,HumanAcc2,AutoAcc)];
+        Tokens = automatedFile.total_tokens(thisDescRowInAutomated);
+        newSubsetTable = [newSubsetTable; table(SubjectID,ObjectID,TalkBlock,HumanAcc1,HumanAcc2,AutoAcc,Tokens)];
     end
 end
 
@@ -46,8 +47,41 @@ a = corrcoef(humanAccAvg,newSubsetTable.AutoAcc(newSubsetTable.TalkBlock == 1));
 % inter-rater
 b = corrcoef(newSubsetTable.HumanAcc1(newSubsetTable.TalkBlock == 1),newSubsetTable.HumanAcc2(newSubsetTable.TalkBlock == 1));
 
+
+close all;
 figure;
-scatter(humanAccAvg,newSubsetTable.AutoAcc(newSubsetTable.TalkBlock == 1),'filled')
-title(['Talk Block 1 human-human correlation: ' num2str(b(2,1))])
-xlabel('Mean Human Rating')
-ylabel('Auto accuracy score')
+    tokens = newSubsetTable.Tokens(newSubsetTable.TalkBlock == 1);
+    markerSizes = rescale(tokens, 30, 1000);
+    scatter(humanAccAvg,newSubsetTable.AutoAcc(newSubsetTable.TalkBlock == 1),tokens,'filled','MarkerFaceAlpha',.5)
+    title(['Talk Block 1 human-human correlation: ' num2str(a(2,1))])
+    xlabel('Mean Human Rating')
+    ylabel('Auto accuracy score')
+    xlim([.5 4.5])
+
+
+
+
+% Make a variable in the automated file that logs human raters where I
+% have them
+
+mean_human_acc = nan([height(automatedFile),1]);
+automatedFile = addvars(automatedFile,mean_human_acc);
+
+for thisDescription = 1:height(ratingsFile)
+    thisDescRowInAutomated = find(...
+        contains(automatedFile.subject_name, ratingsFile.SubjectID(thisDescription)) & ...
+        contains(automatedFile.object_name, ratingsFile.ObjectID(thisDescription)) & ...
+        ratingsFile.TalkBlock(thisDescription) == automatedFile.block_number);
+
+    if isempty(thisDescRowInAutomated)
+        continue
+    else
+        meanHumanAccuracyThisDescription = ...
+            mean([ratingsFile.accuracy_Nicole(thisDescription) ...
+            ratingsFile.accuracy_Tarun(thisDescription)]);
+        automatedFile.mean_human_acc(thisDescRowInAutomated) = meanHumanAccuracyThisDescription;
+    end
+end
+
+writetable(automatedFile,[projectDir ...
+    'data_public/participantDescriptionAccuracy_072225.csv'],'Delimiter',',');
